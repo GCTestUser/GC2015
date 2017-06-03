2e52e1781779f190b7b133421e11e4a1b9462103
GCTestUser <xzgnn2011@yahoo.co.jp>
2017-06-03 18:42:10 +0900

    Unity プロジェクト

---
+++ #03_Unity/Unity/2D20150519/Assets/Script/Enemy1.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy1 : MonoBehaviour
+{
+    new Rigidbody2D rigidbody2D;
+    public int speed = -3;
+    public GameObject explosion;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+     //   Debug.Log("Enemy:Update" + rigidbody2D.velocity);
+     //   Debug.Log("Time.deltaTime" + Time.deltaTime);
+        rigidbody2D.velocity = new Vector2( speed, rigidbody2D.velocity.y);
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("Enemy:col.tag=" + col.tag);
+        if (col.tag == "Bullet")
+        {
+            Destroy(gameObject);
+            Instantiate(explosion, transform.position, transform.rotation);
+        }
+    }
+
+}
