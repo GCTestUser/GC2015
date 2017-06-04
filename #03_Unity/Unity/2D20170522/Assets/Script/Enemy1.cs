fdfb3eda38c92f4419a4846c5eaf86d7c688c55b
GCTestUser <xzgnn2011@yahoo.co.jp>
2017-06-04 13:27:10 +0900

    Add Project

---
+++ #03_Unity/Unity/2D20170522/Assets/Script/Enemy1.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Enemy1 : MonoBehaviour
+{
+    new Rigidbody2D rigidbody2D;
+    public int speed = -3;
+
+    public GameObject explosion;
+
+    public int attackPoint = 10;
+    public Life life;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        rigidbody2D.velocity = new Vector2(speed, rigidbody2D.velocity.y);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Bullet")
+        {
+            Destroy(gameObject);
+            Instantiate(explosion, transform.position, transform.rotation);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //UnityChanとぶつかった時
+        if (col.gameObject.tag == "UnityChan")
+        {
+            //LifeScriptのLifeDownメソッドを実行
+            life.LifeDown(attackPoint);
+        }
+    }
+}
---
+++ #03_Unity/Unity/2D20170522/Assets/Script/Life.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Life : MonoBehaviour
+{
+
+    RectTransform rt;
+
+    void Start()
+    {
+        rt = GetComponent<RectTransform>();
+        Debug.Log("LifeSize="+rt);
+    }
+
+    public void LifeDown(int ap)
+    {
+        //RectTransformのサイズを取得し、マイナスする
+        rt.sizeDelta -= new Vector2(0, ap);
+    }
+}
